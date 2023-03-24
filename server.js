fc6fae8949b6da6fb8f3840d3eb9c18e7c6854e1
Shelwin <shelwinsunga@gmail.com>
2023-03-24 11:20:29 -0500

    yo

---
+++ server.js
@@ -0,0 +1,29 @@
+// server.js
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+let currentContent = '';
+
+io.on('connection', (socket) => {
+    console.log('User connected:', socket.id);
+
+    socket.emit('initialize-content', currentContent);
+
+    socket.on('text-change', (data) => {
+        currentContent = data;
+        socket.broadcast.emit('text-change', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
